8f0bdffd3d12175dcc300a9cdcae1a103e1a7078
apselon <iladam@icloud.com>
2020-09-14 13:38:27 +0300

    Button view

--- GUIController.cpp
+++ GUIController.cpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <SFML/Window/Mouse.hpp>
 #include <cstdio>
 #include <cstdlib>
 #include <SFML/Graphics.hpp>
@@ -7,22 +8,28 @@
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/VideoMode.hpp>
 #include <SFML/Window/Window.hpp>
+#include "GUILib.cpp"
 
 int main(void){
 
 	sf::RenderWindow window(sf::VideoMode(640, 480), "Hellow World");
 
-	auto shape = sf::CircleShape(200);
+	auto button = Button("Hello world", 100, 200);
 
 	while (window.isOpen()){
 		auto event = sf::Event();
 
 		while (window.pollEvent(event)){
 			if (event.type == sf::Event::Closed) window.close();
+			if (event.type == sf::Event::MouseButtonPressed){
+				auto mouse_pos = sf::Mouse::getPosition(window);
+				auto mouse_posf = sf::Vector2f(mouse_pos.x, mouse_pos.y);
+				button.update(mouse_posf);
+			}
 		}
 
 		window.clear();
-		window.draw(shape);
+		window.draw(button.display());
 		window.display();
 	}
 	return 0;
