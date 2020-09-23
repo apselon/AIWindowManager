eec53097a9d352ceabd075d9b50b3c0d320a9201
apselon <iladam@icloud.com>
2020-09-23 15:13:11 +0300

    Added Clickable factory to manage and update gui elements

--- GUILib.cpp
+++ GUILib.cpp
@@ -1,125 +1,136 @@
 #pragma once
 
-#include <SFML/Window/Mouse.hpp>
 #include <cstdio>
 #include <cstdlib>
 #include <assert.h>
+
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Drawable.hpp>
 #include <SFML/Graphics/Font.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderStates.hpp>
 #include <SFML/Graphics/RenderTarget.hpp>
 #include <SFML/Graphics/Text.hpp>
+#include <SFML/System/Vector2.hpp>
 #include <SFML/Window/Event.hpp>
+#include <SFML/Window/Mouse.hpp>
 #include <SFML/Window/VideoMode.hpp>
 #include <SFML/Window/Window.hpp>
 
-class Label {
+#include "GUILib.hpp"
+
+namespace GUILib {
+
+class Clickable: public sf::Drawable {
+public:
+	virtual void handle_event(const sf::Vector2f& mouse_pos) = 0;
+};
+
+class Label: public sf::Drawable {
+
+	friend class Button;
+
 protected:
-	const char* text_buf = nullptr;
-	sf::Text presented_text = sf::Text();
-
-	double pos_x = 0;
-	double pos_y = 0;
-
-	size_t char_size = 12;
+	const char* text_buf      = nullptr;
+	sf::Text presented_text   = sf::Text();
 
 	const char* font_filename = nullptr;
-	sf::Font font = sf::Font();
-
-	sf::Color color = sf::Color();
+	sf::Font font             = sf::Font();
 
 public:
-	Label(const char* text, double x = 0, double y = 0, size_t size = 12, 
-				const char* font_filename = "Misc/SanFrancisco.ttf", 
-				sf::Color color = sf::Color::Black) 
+	Label(const char* text, double x = 0, double y = 0, size_t size = 20, 
+				const char* font_filename = "Misc/SanFranciscoBold.ttf", 
+				sf::Color color = sf::Color::White) 
 
-				:text_buf(text), pos_x(x), pos_y(y), char_size(size), 
-				font_filename(font_filename), color(color) {
+				:text_buf(text), 
+				font_filename(font_filename){
 
 		presented_text.setString(text_buf);
 
 		font.loadFromFile(font_filename);
 
 		presented_text.setFont(font);
-		presented_text.setCharacterSize(char_size);
+		presented_text.setCharacterSize(size);
 
 		presented_text.setFillColor(color);
 		presented_text.setPosition(x, y);
 	}
 
-	void render(sf::RenderTarget& target){
+	void draw(sf::RenderTarget& target, sf::RenderStates) const override{
 
 		target.draw(presented_text);
 	}
-
-	sf::Text display(void){
-		return presented_text;
-	}
 };
 
-class Button: protected Label {
+
+class Button: public Clickable {
 protected:
-	double width  = 0;
-	double height = 0;
-
 	sf::RectangleShape button_box = sf::RectangleShape();
+	Label main_label              = Label(nullptr);
 
-	sf::Color color_active   = sf::Color();
-	sf::Color border_color   = sf::Color();
-	sf::Color color_inactive = sf::Color();
+	sf::Color color_pressed   = sf::Color();
+	sf::Color color_hovered   = sf::Color();
+	sf::Color color_idle      = sf::Color();
 
-	bool is_pressed = 0;
-	bool is_hovered = 0;
-	
+	ButtonState state = BTN_IDLE;
+
 public:
 	Button(const char* text, double width, double height, 
-				double x = 0, double y = 0, size_t font_size = 12,
-				sf::Color color_incactive = sf::Color::White, 
-				sf::Color color_active    = sf::Color::Cyan,
-				sf::Color border_color    = sf::Color::Black)
+				double x = 0, double y = 0, size_t font_size = 15,
+				sf::Color color_pressed   = sf::Color(236, 96, 64),
+				sf::Color color_hovered   = sf::Color(248, 128, 64),
+				sf::Color color_idle      = sf::Color(248, 128, 64),
+				sf::Color border_color    = sf::Color::Transparent,
+				sf::Color text_color      = sf::Color::White)
 
-				:Label(text, x, y, font_size), width(width), height(height),
-				color_active(color_active), border_color(border_color),
-				color_inactive(color_incactive)                                                   {
+				:main_label(Label(text, x, y, font_size, "Misc/SanFranciscoBold.ttf", text_color)), 
+				color_pressed(color_pressed), 
+				color_hovered(color_hovered),
+				color_idle(color_idle)                    { 
 
-		auto text_bounds = Label::presented_text.getLocalBounds();
+		auto text_bounds = main_label.presented_text.getLocalBounds();
 
-		Label::presented_text.setPosition(x + width/2  - text_bounds.width  / 2,
-		                                  y + height/2 - text_bounds.height / 2);
+		main_label.presented_text.setPosition(x + width  / 2  - text_bounds.width  / 2 - text_bounds.left,
+		                                  y + height / 2  - text_bounds.height / 2 - text_bounds.top);
 					
 		button_box.setSize(sf::Vector2f(width, height));		
 		button_box.setPosition(x, y);
-
-		button_box.setFillColor(color_inactive);
-
-		button_box.setOutlineThickness(2);
+		button_box.setOutlineThickness(3);
 		button_box.setOutlineColor(border_color);
 
 	}
 
-	void update(const sf::Vector2f& mouse_pos){
+	void handle_event(const sf::Vector2f& mouse_pos) override {
+
 		if (button_box.getGlobalBounds().contains(mouse_pos)){
-			button_box.setFillColor(color_active);
+
+			state = BTN_HOVERED;
+			button_box.setFillColor(color_hovered);
 
 			if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-				printf("Clicked\n");
+
+				state = BTN_PRESSED;
+				button_box.setFillColor(color_pressed);
 			}
 		}
 
 		else {
-			button_box.setFillColor(color_active);
+
+			state = BTN_IDLE;
+			button_box.setFillColor(color_idle);
 		}
 	}
 
-	void render(sf::RenderTarget& target){
+	int get_state(void){
+		return state;
+	}
+
+	void draw(sf::RenderTarget& target, sf::RenderStates) const override {
 
 		target.draw(button_box);
-		Label::render(target);
-	}
-
-	sf::RectangleShape display(void){
-		return button_box;
+		target.draw(main_label);
 	}
 };
+}
---
+++ GUILib.hpp
@@ -0,0 +1,12 @@
+namespace GUILib {
+
+enum ButtonState {
+	BTN_IDLE,
+	BTN_HOVERED,
+	BTN_PRESSED
+};
+	
+class Clickable;
+class Label;
+class Button;
+};
