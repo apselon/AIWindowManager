112134adcf683e804c6a2515b462bd7c83274098
apselon <iladam@icloud.com>
2020-10-28 21:43:47 +0300

    Added utils

--- Lib/Aux/Auxiliary.hpp
+++ Lib/Aux/Auxiliary.hpp
@@ -1,4 +1,10 @@
+#pragma once
+
 #include <list>
+#include <queue>
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Vector2.hpp>
 
 template <typename T>
 struct Vector2 {
@@ -7,9 +13,11 @@ struct Vector2 {
 
     Vector2() = default;
     Vector2(T x, T y): x(x), y(y) {};
+
 };
 
 using Vector2d = Vector2<double>;
 using Vector2sz = Vector2<unsigned long>;
 
 using std::list;
+using std::queue;
