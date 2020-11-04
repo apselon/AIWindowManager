cc2e3fe12e069eaeb5eb85b4b62bac865317ea30
apselon <iladam@icloud.com>
2020-11-04 23:34:52 +0300

    Rewrited AbstractWindow as a container

--- Lib/Window/Window.cpp
+++ Lib/Window/Window.cpp
@@ -1,46 +1,16 @@
 #include "Window.hpp"
 
-bool AbstractWindow::handle_mouse_click(const Vector2d& click){
-    return on_mouse_click(click);
-}
+bool AbstractWindow::handle_redraw() {
+    for (auto sub: subwindows) {
+        sub->handle_redraw();
+    }
 
-bool AbstractWindow::handle_redraw(){
-    return false; 
-}
-
-bool AbstractWindow::handle_mouse_move(const Vector2d& dest){
-    return on_mouse_move(dest); 
-}
-
-bool AbstractWindow::handle_mouse_release(const Vector2d& m_pos){
-    return on_mouse_release(m_pos);
-}
-
-AbstractWindow::~AbstractWindow(){
-    //PASS;
-}
-
-//================================================================================
-
-bool AbstractRenderWindow::handle_redraw(){
-    draw_at();
     return true;
 }
 
-AbstractRenderWindow::~AbstractRenderWindow(){
-    //PASS;
-}
-
-//================================================================================
-
-void AbstractContainerWindow::add_subwindow(AbstractWindow* another) {
-    subwindows.push_back(another);
-}
-
-bool AbstractContainerWindow::handle_mouse_click(const Vector2d& click) {
-
-    for (auto sub: subwindows){
-        if(sub->handle_mouse_click(click)){
+bool AbstractWindow::handle_mouse_click(const Vector2d& click) {
+    for (auto sub: subwindows) {
+        if(sub->handle_mouse_click(click)) {
             return true;
         }
     }
@@ -48,10 +18,9 @@ bool AbstractContainerWindow::handle_mouse_click(const Vector2d& click) {
     return on_mouse_click(click);
 }
 
-bool AbstractContainerWindow::handle_mouse_move(const Vector2d& dest) {
-
-    for (auto sub: subwindows){
-        if(sub->handle_mouse_move(dest)){
+bool AbstractWindow::handle_mouse_move(const Vector2d& dest) {
+    for (auto sub: subwindows) {
+        if(sub->handle_mouse_move(dest)) {
             return true;
         }
     }
@@ -59,39 +28,36 @@ bool AbstractContainerWindow::handle_mouse_move(const Vector2d& dest) {
     return on_mouse_move(dest);
 }
 
-bool AbstractContainerWindow::handle_redraw(){
-    for (auto sub: subwindows){
-        sub->handle_redraw();
-    }
-
-    return true;
-}
-
-bool AbstractContainerWindow::handle_mouse_release(const Vector2d& m_pos){
-    for (auto sub: subwindows){
+bool AbstractWindow::handle_mouse_release(const Vector2d& m_pos) {
+    for (auto sub: subwindows) {
         sub->handle_mouse_release(m_pos);
     }
 
     return on_mouse_release(m_pos);
 }
 
-AbstractContainerWindow::~AbstractContainerWindow() {
-    for (auto sub: subwindows){
+void AbstractWindow::add_subwindow(AbstractWindow* another) {
+    subwindows.push_back(another);
+}
+
+AbstractWindow::~AbstractWindow() {
+    for (auto sub: subwindows) {
+        sub->~AbstractWindow();
         delete sub;
-    };
+        sub = nullptr;
+    }
 }
 
 //================================================================================
 
-/*
-bool AbstractRenderContainerWindow::handle_redraw(){
-    draw_at();
-
-    for (auto sub: subwindows){
+bool AbstractRenderWindow::handle_redraw() {
+    for (auto sub: subwindows) {
         sub->handle_redraw();
     }
 
-    return on_redraw();
-    
+    on_redraw();
+    return true;
 }
-*/
+
+//================================================================================
+
--- Lib/Window/Window.hpp
+++ Lib/Window/Window.hpp
@@ -14,10 +14,13 @@ class AbstractWindow {
     friend AbstractRenderWindow;
     friend AbstractContainerWindow;
 
+private:
+    list<AbstractWindow*> subwindows = list<AbstractWindow*>();
+
 protected:
-    virtual bool on_mouse_click(const Vector2d& click) = 0;
-    virtual bool on_mouse_move (const Vector2d& dest)  = 0;
-    virtual bool on_mouse_release    (const Vector2d& pos)   = 0;
+    virtual bool on_mouse_click  (const Vector2d& click) = 0;
+    virtual bool on_mouse_move   (const Vector2d& dest)  = 0;
+    virtual bool on_mouse_release(const Vector2d& pos)   = 0;
     /*
     virtual bool handle_timer(time_t timer)  = 0;
     virtual bool handle_resize(size_t new_w, size_t new_h)  = 0;
@@ -25,54 +28,27 @@ protected:
     */
 
 public: 
-    virtual bool handle_mouse_release(const Vector2d& click);
     virtual bool handle_mouse_click  (const Vector2d& click);
     virtual bool handle_mouse_move   (const Vector2d& dest); 
+    virtual bool handle_mouse_release(const Vector2d& click);
     virtual bool handle_redraw();
+
+    virtual void add_subwindow(AbstractWindow* another);
+
     virtual ~AbstractWindow();
 };
 
 //================================================================================
 
 class AbstractRenderWindow: public AbstractWindow {
-/*!
- * Окно, которое может быть нарисовано внутри другого окна.
- */
-friend AbstractContainerWindow;
+    friend AbstractContainerWindow;
 
-public:
-    bool handle_redraw() override;
-    virtual void draw_at() = 0;
-    virtual ~AbstractRenderWindow();
-};
-
-//================================================================================
-
-class AbstractContainerWindow: public AbstractWindow {
-//Winndow that sends its events to subwindows.
 protected:
-    list<AbstractWindow*> subwindows = list<AbstractWindow*>();
+    virtual void on_redraw() = 0;
 
 public:
-    AbstractContainerWindow() = default;
-    AbstractContainerWindow(const AbstractContainerWindow& another);
-    AbstractContainerWindow(AbstractContainerWindow&& another);
-
-    virtual void add_subwindow(AbstractWindow* another);
-    bool handle_mouse_release(const Vector2d& m_pos) override;
-    bool handle_mouse_click  (const Vector2d& click) override;
-    bool handle_mouse_move   (const Vector2d& dest)  override; 
     bool handle_redraw() override;
-
-    virtual  ~AbstractContainerWindow();
+    //virtual ~AbstractRenderWindow();
 };
-
-//================================================================================
-
-/*
-class AbstractRenderContainerWindow: AbstractContainerWindow, AbstractRenderWindow {
-    bool handle_redraw() override;
-};
-*/
 
 //================================================================================
