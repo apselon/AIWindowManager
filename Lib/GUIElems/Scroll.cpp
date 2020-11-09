f113de73abf3581f72781fdc143a15c3e0555268
apselon <iladam@icloud.com>
2020-11-09 22:23:59 +0300

    Added directed and limited slider

--- Lib/GUIElems/Scroll.cpp
+++ Lib/GUIElems/Scroll.cpp
@@ -1,3 +1,25 @@
 #include "Scroll.hpp"
 #include "../Engine/SFMLGraphics.hpp"
 
+Slider::Slider(const Vector2d& pos, const Vector2d& size, const Vector2d& limits,
+               const Orientation orientation)
+    :DraggableRectWindow(pos, size), limits(limits), orientation(orientation)
+{
+    dir = {0, 1};
+}
+
+void Slider::drag_to(const Vector2d& click) {
+    auto new_pos = pos + (dir * ((click - pos - drag_rel_pos) ^ dir));
+
+    switch (orientation){
+        case Orientation::VERTICAL:
+            if (new_pos.y <= limits.x) new_pos.y = limits.x;
+            if (new_pos.y >= limits.y) new_pos.y = limits.y;
+            break;
+            
+        default:
+            break;
+    }
+
+    pos = new_pos;
+}
--- Lib/GUIElems/Scroll.hpp
+++ Lib/GUIElems/Scroll.hpp
@@ -1,2 +1,21 @@
 #include "../Window/Window.hpp"
 
+enum class Orientation {
+    VERTICAL,
+    HORIZONTAL,
+    DIAGONAL,
+    OR_LENGTH
+};
+
+class Slider: public DraggableRectWindow {
+protected:
+    Vector2d dir = {};
+    Vector2d limits = {};
+    Orientation orientation = Orientation::VERTICAL;
+
+public:
+    Slider(const Vector2d& pos, const Vector2d& size, const Vector2d& limits,
+           const Orientation orientation = Orientation::VERTICAL); 
+
+    void drag_to(const Vector2d& click) override;
+};
