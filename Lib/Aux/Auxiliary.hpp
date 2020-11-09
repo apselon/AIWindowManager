11686d93daa52f907db693a426e715a6f073a354
apselon <iladam@icloud.com>
2020-11-09 21:28:06 +0300

    Added Vector manipulations

--- Lib/Aux/Auxiliary.hpp
+++ Lib/Aux/Auxiliary.hpp
@@ -2,9 +2,12 @@
 
 #include <list>
 #include <queue>
+#include <cmath>
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Vector2.hpp>
+
+constexpr double EPS = 0.1;
 
 template <typename T>
 struct Vector2 {
@@ -14,10 +17,29 @@ struct Vector2 {
     Vector2() = default;
     Vector2(T x, T y): x(x), y(y) {};
 
+    bool operator ||(const Vector2& another) const {
+        return fabs(x * another.y - another.x * y) <= EPS;    
+    }
+
+    Vector2 operator -(const Vector2& another) const {
+        return {x - another.x, y - another.y};
+    }
+
+    Vector2 operator +(const Vector2& another) const {
+        return {x + another.x, y + another.y};
+    }
+
+    T operator ^(const Vector2& another) const {
+        return x * another.x +  y * another.y;
+    }
+
+    Vector2 operator *(const T& mul) const {
+        return {x * mul, y * mul};
+    }
 };
 
 using Vector2d = Vector2<double>;
-using Vector2sz = Vector2<unsigned long>;
+//using Vector2sz = Vector2<double>;
 
 using std::list;
 using std::queue;
