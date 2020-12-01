e9946c679d46ca6638263f943b9ec0f07e6f3793
apselon <iladam@icloud.com>
2020-12-01 21:57:26 +0300

    Minor

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "Lib/Engine/Application.hpp"
 #include "Lib/GUIElems/Scroll.hpp"
+#include "Lib/GUIElems/Button.hpp"
 
 int main(void){
     AIWM::Application::start();
     AIWM::Application::open_window(new Slider({100 + 100, 100}, {10, 10}, {100, 100 + 100}, nullptr));
+    AIWM::Application::open_window(new Slider({100 + 200, 100}, {10, 10}, {100, 100 + 100}, nullptr));
     AIWM::Application::run();
     AIWM::Application::stop();
 };
