bcf851bbc045105f4b08d3626c2cf9bb9210deb5
apselon <iladam@icloud.com>
2020-12-14 22:24:38 +0300

    Ok.

--- main.cpp
+++ main.cpp
@@ -6,11 +6,13 @@
 int main(void){
     AIWM::Application::start();
 
-    auto scroll_view = new ScrollView({10, 10}, {100, 100});
+    auto scroll_view = new ScrollView({10, 10}, {200, 100});
     scroll_view->add_subwindow(new TextLabel({0, 0}, {20, 20}, "Hello World!", 40));
     //scroll_view->add_subwindow(new HelloWorldButton({0, 0}, {20, 20}));
     AIWM::Application::open_window(scroll_view);
     AIWM::Application::open_window(new Slider({100 + 200, 100}, {10, 10}, {100, 100 + 100}, scroll_view));
+
+    AIWM::Application::open_window(new HelloWorldButton({400, 400}, {100, 100}, "Misc/Images/Floppa.jpg"));
 
 
     AIWM::Application::run();
